d7f9c0136d09cfbc681fa580e8469843215e1ae6
Mutiara Widdi <mutiewiddia@yahoo.co.id>
2020-11-15 13:13:30 +0700

    My Fourth Commit

--- app/Views/Mahasiswa/header.php
+++ app/Views/Mahasiswa/header.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Praktikum 4</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mahasiswa</title>
 </head>
